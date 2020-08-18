5ec38fe6cb89f50e8a2ffce60e5a7a69e1074776
JamieMann <support@thepihut.com>
2020-08-18 15:12:06 +0100

    Create simplepircode.py

---
+++ examples/simple-pir-code/simplepircode.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+PIR_PIN = 17
+GPIO.setup(PIR_PIN, GPIO.IN)
+
+try:
+	print “PIR Module Test (CTRL+C to exit)”
+	time.sleep(2)
+	print “Ready”
+	while True:
+		if GPIO.input(PIR_PIN):
+			print “Motion Detected!”
+		time.sleep(1)
+
+except KeyboardInterrupt:
+	print “ Quit”
+	GPIO.cleanup()
